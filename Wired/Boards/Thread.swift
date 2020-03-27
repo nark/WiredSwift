fffaa6e2679f6ea79bea3925fbf163b10a40061b
Rafaël Warnault <rw@read-write.fr>
2020-03-27 22:18:05 +0100

    Thread row has dynamic height

--- Wired/Boards/Thread.swift
+++ Wired/Boards/Thread.swift
@@ -9,9 +9,13 @@
 import Cocoa
 
 public class Thread: ConnectionObject {
+    public var uuid:String!
     public var subject:String!
     public var nick:String!
-    
+    public var replies:Int!
+    public var postDate:Date!
+    public var editDate:Date!
+    public var lastReplyDate:Date!
     public var board:Board!
     
     init(_ message: P7Message, board: Board, connection: ServerConnection) {
@@ -19,12 +23,32 @@ public class Thread: ConnectionObject {
         
         self.board = board
         
+        if let p = message.string(forField: "wired.board.thread") {
+            self.uuid = p
+        }
+        
         if let p = message.string(forField: "wired.board.subject") {
             self.subject = p
         }
         
         if let p = message.string(forField: "wired.user.nick") {
             self.nick = p
+        }
+        
+        if let p = message.date(forField: "wired.board.post_date") {
+            self.postDate = p
+        }
+        
+        if let p = message.date(forField: "wired.board.edit_date") {
+            self.editDate = p
+        }
+        
+        if let p = message.date(forField: "wired.board.latest_reply_date") {
+            self.lastReplyDate = p
+        }
+        
+        if let p = message.uint32(forField: "wired.board.replies") {
+            self.replies = Int(p)
         }
 
     }
