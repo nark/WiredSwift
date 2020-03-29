83ac5d00a279d37b47c28b177877b4d3cd2d9a81
Rafaël Warnault <rw@read-write.fr>
2020-03-29 21:49:59 +0200

    Add user info panel

---
+++ Wired/Extensions/WiredColors.swift
@@ -0,0 +1,9 @@
+//
+//  WiredColors.swift
+//  Wired
+//
+//  Created by Rafael Warnault on 29/03/2020.
+//  Copyright © 2020 Read-Write. All rights reserved.
+//
+
+import Foundation
---
+++ Wired/Users/UserInfoViewController.swift
@@ -0,0 +1,13 @@
+//
+//  UserInfoViewController.swift
+//  Wired
+//
+//  Created by Rafael Warnault on 29/03/2020.
+//  Copyright © 2020 Read-Write. All rights reserved.
+//
+
+import UIKit
+
+class UserInfoViewController: NSViewController {
+
+}
