feb7ea97ef3cb5e1f78eaa266ddd11c36af9151f
Rafaël Warnault <rw@read-write.fr>
2020-04-19 19:55:15 +0200

    Better package structure

--- Carthage/Checkouts/SwiftyRSA/Podfile
+++
@@ -1,8 +0,0 @@
-platform :ios, '8.3'
-install! 'cocoapods', :integrate_targets => false
-
-pod 'SwiftLint', '0.27.0'
-
-target 'SwiftyRSA iOS'
-target 'SwiftyRSA watchOS'
-target 'SwiftyRSA tvOS'
--- Package.swift
+++ Package.swift
@@ -1,11 +1,5 @@
 // swift-tools-version:5.2
-//
-//  Package.swift
-//  WiredSwift
-//
-//  Created by Rafael Warnault on 17/04/2020.
-//  Copyright © 2020 Read-Write. All rights reserved.
-//
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -13,10 +7,13 @@ var dependencies: [Package.Dependency] = []
 var targetDependencies: [Target.Dependency] = []
 
     
+
 #if os(Linux)
     dependencies.append(.package(url: "https://github.com/IBM-Swift/OpenSSL.git", from: "2.2.0"))
     targetDependencies.append(.byName(name: "OpenSSL"))
 #endif
+
+
 
 dependencies.append(.package(name: "AEXML", url: "https://github.com/tadija/AEXML.git", from: "4.5.0"))
 dependencies.append(.package(name: "CryptoSwift", url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.3.0"))
@@ -25,6 +22,8 @@ dependencies.append(.package(name: "CZlib", url: "https://github.com/IBM-Swift/C
 dependencies.append(.package(name: "Gzip", url: "https://github.com/1024jp/GzipSwift", from: "5.1.1"))
 dependencies.append(.package(name: "SocketSwift", url: "https://github.com/BiAtoms/Socket.swift.git", from: "2.4.0"))
 
+
+
 targetDependencies.append(.byName(name: "AEXML"))
 targetDependencies.append(.byName(name: "CryptoSwift"))
 targetDependencies.append(.byName(name: "CryptorRSA"))
@@ -32,21 +31,23 @@ targetDependencies.append(.byName(name: "CZlib"))
 targetDependencies.append(.byName(name: "Gzip"))
 targetDependencies.append(.byName(name: "SocketSwift"))
 
+
+
+
 let package = Package(
     name: "WiredSwift",
-    platforms: [
-        // Add support for all platforms starting from a specific version.
-        .macOS(.v10_13),
-        .iOS(.v12)
-    ],
     products: [
         .library(
             name: "WiredSwift",
-            targets: ["WiredSwift"])
-            
+            targets: ["WiredSwift"]),
     ],
     dependencies: dependencies,
     targets: [
-        .target(name: "WiredSwift", dependencies: targetDependencies)
+        .target(
+            name: "WiredSwift",
+            dependencies: targetDependencies),
+        .testTarget(
+            name: "WiredSwiftTests",
+            dependencies: ["WiredSwift"]),
     ]
 )
