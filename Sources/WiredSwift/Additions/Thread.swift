810ac7e53d2d406ab655573ad718beb1917cc378
Rafaël Warnault <rw@read-write.fr>
2021-04-24 23:15:22 +0200

    Testing basic semaphores (3)

--- Sources/WiredSwift/Additions/Thread.swift
+++ Sources/WiredSwift/Additions/Thread.swift
@@ -15,9 +15,12 @@ extension Thread {
             return "OperationQueue: \(currentOperationQueue)"
         } else if let underlyingDispatchQueue = OperationQueue.current?.underlyingQueue?.label {
             return "DispatchQueue: \(underlyingDispatchQueue)"
-        } else {
+        }
+        #if os(iOS) || os(macOS)
             let name = __dispatch_queue_get_label(nil)
             return String(cString: name, encoding: .utf8) ?? Thread.current.description
-        }
+        #else
+            return "[Unknow thread]"
+        #endif
     }
 }
