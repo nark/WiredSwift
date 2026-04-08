1f2ecf69f1dccf8560a2d49b3666aaa8b1ff4b16
Rafaël Warnault <rw@opale.pro>
2026-04-08 20:21:11 +0200

    chore(release): sync server version 3.0+36

--- Sources/wired3/Core/Version.swift
+++ Sources/wired3/Core/Version.swift
@@ -2,7 +2,7 @@ import Foundation
 
 public enum WiredServerVersion {
     public static let marketingVersion = "3.0"
-    public static let buildNumber = "35"
+    public static let buildNumber = "36"
     public static let commit = "local"
     public static let number = marketingVersion
     public static let display = "wired3 \(marketingVersion) (\(buildNumber)+\(commit))"
